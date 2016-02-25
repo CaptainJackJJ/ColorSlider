c9a03cdcb6fd7543f6f698fbff43ea88a1869cb3
jun.jiang <1669266647@qq.com>
2016-02-25 22:21:14 +0800

    add web test file

---
+++ webtest/current/index.php
@@ -0,0 +1,145 @@
+﻿
+<!DOCTYPE html >
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>兔子影音官方网站</title>
+<link rel="icon" href="logo.ico"/> 
+<link href="style/style.css" type="text/css" rel="stylesheet" />
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?2d875a75ceeaaa21793d63db5aae4cc9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+</head>
+
+<body>
+<div class="main">
+    <div class="wid960" style="height:603px;">
+        <div class="header">
+            <a class="logo" title="兔子影音">
+        </div>
+        <div class="mainTop">
+            <a href="http://rabbitplayer.cn/RPlayerSetup.exe" class="downBtn"></a>
+        </div>
+
+	<div class="bdshare_small fifth">
+         	<!-- Baidu Button BEGIN -->
+		<div class="shareBtn">
+                    <div class="bdsharebuttonbox">
+                        <a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a>			
+                        <a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a>
+                        <a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a>
+			<a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a>
+                        <a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a>   
+			<a href="#" class="bds_tieba" data-cmd="tieba" title="分享到百度贴吧"></a>                      
+                    	<a href="#" class="bds_more" data-cmd="more"></a>
+                    </div>
+                </div>
+		<!-- Baidu Button END -->
+	</div>
+
+	<div class="shareText">分享给好友</div>
+
+<script>
+	window._bd_share_config = {
+		common : {
+			bdText : '分享一款看片神器，电脑版下载地址：rabbitplayer.com，用它可获取最新的未删减版超清电影，有些甚至电影院都还未首映或在国内永远无法上映。',	
+			bdDesc : '很酷很强大的一款万能播放器。',	
+			bdUrl : 'http://rabbitplayer.com', 	
+			bdPic : 'http://www.rabbitplayer.cn/style/img/share.png'
+		},
+		share : [{
+			"bdSize" : 32
+		}],
+		slide : [{	   
+			bdImg : 0,
+			bdPos : "right",
+			bdTop : 100
+		}],
+		image : [{
+			viewType : 'list',
+			viewPos : 'top',
+			viewColor : 'black',
+			viewSize : '16',
+			viewList : ['qzone','tsina','huaban','tqq','renren']
+		}],
+		selectShare : [{
+			"bdselectMiniList" : ['qzone','tqq','kaixin001','bdxc','tqf']
+		}]
+	}
+	with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion='+~(-new Date()/36e5)];
+</script>
+
+    </div>
+</div>
+
+<div class="whatjm">
+	<div class="wid960">
+    </div>
+</div>
+<div class="functionCon">
+	<div class="wid960">
+    	<h3>功能特性</h3>
+    	<ul>
+                <li class="m01">
+                	<span>超高清品质</span><br>
+                   第一时间推送最新的未删减版超高清电影下载资源。普通的在线电影画质不高、某些内容被删减，还有烦人的广告...越来越多的人选择下载观看，内容原滋原味，无任何广告影响观影体验。兔子影音拥有国际一流的播放内核，完美支持4K，1080P等超高清电影，远远优于暴风影音等一般播放器！
+
+                </li>
+                <li class="m02">
+                	<span>百变字幕</span><br>
+                    支持实时调整字幕的颜色、大小、位置等。支持srt、ass等各种类型的内置、外置字幕！
+
+                </li>
+                <li class="m03">
+                	<span>万能播放</span><br>
+                   除了像一般万能播放器一样支持所有文件源格式（mp4、mkv、avi、wmv...），兔子影音还支持ISO、DVD、BD蓝光等！
+
+                </li>
+                <div class="clear"></div>
+
+            	<li class="m04">     
+			<span>助学神器</span><br>         	
+                    有时我们在看美剧、舞蹈比赛、教学等视频时，觉得发音或动作太快，想让其说或跳得慢一丁点。兔子影音为此专门开发了变速播放。让您可以选择...0.6...1.4...倍速播放。更强大的是，变速后的声音依然是原声调。一般播放器即使能变速，但声音会变得怪怪的。保持原调的变速就好似老师特意慢慢地说慢慢地跳一样，给您一种非常自然奇妙的体验！　　　　　　　　　　　　　　　　　　　
+                </li>
+
+                <li class="m05">
+                	<span>智能人性</span><br>
+                    兔子影音的功能都经过精心设计，让用户使用起来更顺心。例如为用户自动创建播放列表，并标记为未看、已看、已看完，让您轻松管理海量电影！
+
+                </li>
+                <li class="m06">
+                	<span>免费安全无广告</span><br>
+                    绿色软件无任何广告，即使在注册到您电脑前都会先征求您的许可！
+                </li>
+            </ul>
+    </div>
+</div>
+<div class="clear"></div>
+
+<div class="clear"></div>
+<div class="footer">
+	<div class="wid960 bdTop tc ">  	
+Copyright &copy; 2015 rabbitplayer.com All Rights Rserved.    QQ：3029825453  
+        </div>
+</div>
+<script type="text/javascript" src="http://www.wmzhe.com/static/js/jquery-1.7.2.min.js" ></script>
+<script>
+$.ajax({
+   'url':'/ver.php',
+   'dataType':'json',
+   'success':function(data){
+		$("#version").html(data['version']);
+		$("#size").html(data['size']);
+		$("#utime").html(data['utime']);
+   }
+});
+</script>
+</body>
+</html>
